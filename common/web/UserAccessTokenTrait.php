73dc978ade5962887d1c1c229f0e4fdeb8d44327
viyt28 <vinh28193@gmail.com>
2017-07-23 21:59:41 +0700

    trait

---
+++ common/web/UserAccessTokenTrait.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\common\web;
+
+use Yii;
+use yii\web\Cookie;
+use yii\web\IdentityInterface;
+use yii\base\InvalidConfigException;
+use yii\base\InvalidParamException;
+
+trait UserAccessTokenTrait
+{
+	private $_accessToken;
+}
+ ?>
