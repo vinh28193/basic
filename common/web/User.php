4d40cb5b65a06059dd91906b8b23bb4a3998f74e
Vinh <vinh28193@gmail.com>
2017-10-12 12:55:49 +0700

    profile

--- common/web/User.php
+++ common/web/User.php
@@ -2,9 +2,8 @@
 namespace app\common\web;
 
 use Yii;
-use yii\db\Query;
-use yii\di\Instance;
-use yii\db\Connection;
+use yii\helpers\ArrayHelper;
+use yii\web\IdentityInterface;
 use yii\base\InvalidConfigException;
 
 /**
@@ -30,47 +29,18 @@ use yii\base\InvalidConfigException;
  */
 class User extends \yii\web\User
 {
-	/**
-     * @var Connection|array|string the DB connection object or the application component ID of the DB connection.
-     * After the DbSession object is created, if you want to change this property, you should only assign it
-     * with a DB connection object.
-     * Starting from version 2.0.2, this can also be a configuration array for creating the object.
-     */
-    public $db = 'db';
 
     /**
-     * @var string the name of the DB table that user access in system.
-     * The table should be pre-created as follows:
-     *
-     * ```sql
-     * CREATE TABLE user_log
-     * (
-     *     id CHAR(40) NOT NULL PRIMARY KEY,
-     *     manager_id INTEGER,
-     *     data BLOB
-     * )
-     * ```
-     *
-     * where 'BLOB' refers to the BLOB-type of your preferred DBMS. Below are the BLOB type
-     * that can be used for some popular DBMS:
-     *
-     * - MySQL: LONGBLOB
-     * - PostgreSQL: BYTEA
-     * - MSSQL: BLOB
-     *
+     * @inheritdoc
      */
-    public $userAccessTable = '{{%user_log}}';
-
-
-	/**
-     * Initializes the DbSession component.
-     * This method will initialize the [[db]] property to make sure it refers to a valid DB connection.
-     * @throws InvalidConfigException if [[db]] is invalid.
-     */
-    public function init()
+    public function behaviors()
     {
-        parent::init();
-        $this->db = Instance::ensure($this->db, Connection::className());
+        return ArrayHelper::merge(
+            parent::behaviors(),[
+
+            ]
+        );
     }
+
 }
  ?>
