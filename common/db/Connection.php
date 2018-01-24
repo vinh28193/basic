56990b555f0c63ea3b9c34a2ad107e5cbc19fc16
viyt28 <vinh28193@gmail.com>
2018-01-24 21:10:12 +0700

    formater

--- common/db/Connection.php
+++ common/db/Connection.php
@@ -1,21 +1,24 @@
 <?php
+
 namespace app\common\db;
 
 /**
  * Connection is the class behind the \yii\db\Connection represents a connection to a database via [PDO]
  */
  class Connection extends \yii\db\Connection
- {
+ {	
  	
  	/**
      * Initializes the Connection
      */
  	public function init()
     {
+    	parent::init();
         //merge schema map
         $this->schemaMap = \yii\helpers\ArrayHelper::merge($this->schemaMap, [
         	'mysql' => 'app\common\db\mysql\Schema', // MySQL
         ]);
-        parent::init();
+        
     }
- } 
+}
+?>
