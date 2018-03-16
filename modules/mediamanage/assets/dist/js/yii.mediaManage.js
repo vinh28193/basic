e89fd2b847b6b90b083b8d177abf43cbe29f94e4
viyt28 <vinh28193@gmail.com>
2018-03-16 20:26:40 +0700

    Todo Refactor

--- modules/mediamanage/assets/dist/js/yii.mediaManage.js
+++ modules/mediamanage/assets/dist/js/yii.mediaManage.js
@@ -3,3 +3,246 @@ yii.mediaManage =  (function ($) {
 
    
 })(jQuery);
+(function ($) {
+    $.fn.yiiMediaManage = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist in jQuery.yiiMediaManage');
+            return false;
+        }
+    };
+
+    var defaults = {
+        filterUrl: undefined,
+        filterSelector: undefined
+    };
+
+    var mediaData = {};
+
+    var mediaEvents = {
+        /**
+         * beforeFilter event is triggered before filtering the media.
+         * The signature of the event handler should be:
+         *     function (event)
+         * where
+         *  - event: an Event object.
+         *
+         * If the handler returns a boolean false, it will stop filter form submission after this event. As
+         * a result, afterFilter event will not be triggered.
+         */
+        beforeFilter: 'beforeFilter',
+        /**
+         * afterFilter event is triggered after filtering the media and filtered results are fetched.
+         * The signature of the event handler should be:
+         *     function (event)
+         * where
+         *  - event: an Event object.
+         */
+        afterFilter: 'afterFilter'
+    };
+
+    /**
+     * Used for storing active event handlers and removing them later.
+     * The structure of single event handler is:
+     *
+     * {
+     *     mediaViewId: {
+     *         type: {
+     *             event: '...',
+     *             selector: '...'
+     *         }
+     *     }
+     * }
+     *
+     * Used types:
+     *
+     * - filter, used for filtering media with elements found by filterSelector
+     * - checkRow, used for checking single row
+     * - checkAllRows, used for checking all rows with according "Check all" checkbox
+     *
+     * event is the name of event, for example: 'change.yiimediaView'
+     * selector is a jQuery selector for finding elements
+     *
+     * @type {{}}
+     */
+    var mediaEventHandlers = {};
+
+    var methods = {
+        init: function (options) {
+            return this.each(function () {
+                var $e = $(this);
+                var settings = $.extend({}, defaults, options || {});
+                var id = $e.attr('id');
+                if (mediaData[id] === undefined) {
+                    mediaData[id] = {};
+                }
+
+                mediaData[id] = $.extend(mediaData[id], {settings: settings});
+                console.log(mediaData);
+                var filterEvents = 'change.yiiMediaManage keydown.yiiMediaManage';
+                var enterPressed = false;
+                initEventHandler($e, 'filter', filterEvents, settings.filterSelector, function (event) {
+                    if (event.type === 'keydown') {
+                        if (event.keyCode !== 13) {
+                            return; // only react to enter key
+                        } else {
+                            enterPressed = true;
+                        }
+                    } else {
+                        // prevent processing for both keydown and change events
+                        if (enterPressed) {
+                            enterPressed = false;
+                            return;
+                        }
+                    }
+
+                    methods.applyFilter.apply($e);
+
+                    return false;
+                });
+            });
+        },
+
+        applyFilter: function () {
+            var $media = $(this);
+            var settings = mediaData[$media.attr('id')].settings;
+            var data = {};
+            $.each($(settings.filterSelector).serializeArray(), function () {
+                if (!(this.name in data)) {
+                    data[this.name] = [];
+                }
+                data[this.name].push(this.value);
+            });
+
+            var namesInFilter = Object.keys(data);
+
+            $.each(yii.getQueryParams(settings.filterUrl), function (name, value) {
+                if (namesInFilter.indexOf(name) === -1 && namesInFilter.indexOf(name.replace(/\[\d*\]$/, '')) === -1) {
+                    if (!$.isArray(value)) {
+                        value = [value];
+                    }
+                    if (!(name in data)) {
+                        data[name] = value;
+                    } else {
+                        $.each(value, function (i, val) {
+                            if ($.inArray(val, data[name])) {
+                                data[name].push(val);
+                            }
+                        });
+                    }
+                }
+            });
+
+            var pos = settings.filterUrl.indexOf('?');
+            var url = pos < 0 ? settings.filterUrl : settings.filterUrl.substring(0, pos);
+            var hashPos = settings.filterUrl.indexOf('#');
+            if (pos >= 0 && hashPos >= 0) {
+                url += settings.filterUrl.substring(hashPos);
+            }
+
+            $media.find('form.media-filter-form').remove();
+            var $form = $('<form/>', {
+                action: url,
+                method: 'get',
+                'class': 'media-filter-form',
+                style: 'display:none',
+                'data-pjax': ''
+            }).appendTo($media);
+            $.each(data, function (name, values) {
+                $.each(values, function (index, value) {
+                    $form.append($('<input/>').attr({type: 'hidden', name: name, value: value}));
+                });
+            });
+
+            var event = $.Event(mediaEvents.beforeFilter);
+            $media.trigger(event);
+            if (event.result === false) {
+                return;
+            }
+
+            $form.submit();
+
+            $media.trigger(mediaEvents.afterFilter);
+        },
+
+        setSelectionColumn: function (options) {
+            var $media = $(this);
+            var id = $(this).attr('id');
+            if (mediaData[id] === undefined) {
+                mediaData[id] = {};
+            }
+            mediaData[id].selectionColumn = options.name;
+            if (!options.multiple || !options.checkAll) {
+                return;
+            }
+            var checkAll = "#" + id + " input[name='" + options.checkAll + "']";
+            var inputs = options['class'] ? "input." + options['class'] : "input[name='" + options.name + "']";
+            var inputsEnabled = "#" + id + " " + inputs + ":enabled";
+            initEventHandler($media, 'checkAllRows', 'click.yiiMediaManage', checkAll, function () {
+                $media.find(inputs + ":enabled").prop('checked', this.checked);
+            });
+            initEventHandler($media, 'checkRow', 'click.yiiMediaManage', inputsEnabled, function () {
+                var all = $media.find(inputs).length == $media.find(inputs + ":checked").length;
+                $media.find("input[name='" + options.checkAll + "']").prop('checked', all);
+            });
+        },
+
+        getSelectedRows: function () {
+            var $media = $(this);
+            var data = mediaData[$media.attr('id')];
+            var keys = [];
+            if (data.selectionColumn) {
+                $media.find("input[name='" + data.selectionColumn + "']:checked").each(function () {
+                    keys.push($(this).parent().closest('tr').data('key'));
+                });
+            }
+
+            return keys;
+        },
+
+        destroy: function () {
+            var events = ['.yiiMediaManage', mediaEvents.beforeFilter, mediaEvents.afterFilter].join(' ');
+            this.off(events);
+
+            var id = $(this).attr('id');
+            $.each(mediaEventHandlers[id], function (type, data) {
+                $(document).off(data.event, data.selector);
+            });
+
+            delete mediaData[id];
+
+            return this;
+        },
+
+        data: function () {
+            var id = $(this).attr('id');
+            return mediaData[id];
+        }
+    };
+
+    /**
+     * Used for attaching event handler and prevent of duplicating them. With each call previously attached handler of
+     * the same type is removed even selector was changed.
+     * @param {jQuery} $mediaManager According jQuery media view element
+     * @param {string} type Type of the event which acts like a key
+     * @param {string} event Event name, for example 'change.yiimediaView'
+     * @param {string} selector jQuery selector
+     * @param {function} callback The actual function to be executed with this event
+     */
+    function initEventHandler($mediaManager, type, event, selector, callback) {
+        var id = $mediaManager.attr('id');
+        var prevHandler = mediaEventHandlers[id];
+        if (prevHandler !== undefined && prevHandler[type] !== undefined) {
+            var data = prevHandler[type];
+            $(document).off(data.event, data.selector);
+        }
+        if (prevHandler === undefined) {
+            mediaEventHandlers[id] = {};
+        }
+        $(document).on(event, selector, callback);
+        mediaEventHandlers[id][type] = {event: event, selector: selector};
+    }
+})(window.jQuery);
--- modules/mediamanage/views/default/index.php
+++ modules/mediamanage/views/default/index.php
@@ -1,12 +1,168 @@
-<div class="filemanager-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+<div class="media-manager">
+    <section class="container-fluid media-grid-view">
+        <div class="row filter">
+            <div class="col-md-12">
+                <form class="form-inline">
+                    <div class="form-group">
+                        <label for="exampleInputName2">Name</label>
+                        <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail2">Email</label>
+                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+                    </div>
+                  <button type="submit" class="btn btn-default">Send invitation</button>
+                </form>
+            </div>   
+        </div>
+        <div class="row items">
+            <div class="col-md-12">
+                <div class="row">
+
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Koala.jpg" alt="...">
+                        </a>
+                    </div>
+
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Chrysanthemum.jpg" alt="...">
+                        </a>
+                    </div>
+    
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Desert.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Hydrangeas.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Jellyfish.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Koala.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Lighthouse.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Penguins.jpg" alt="...">
+                        </a>
+                    </div>
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Tulips.jpg" alt="...">
+                        </a>
+                  </div>
+
+                   <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Koala.jpg" alt="...">
+                        </a>
+                    </div>
+
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Chrysanthemum.jpg" alt="...">
+                        </a>
+                    </div>
+    
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Desert.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Hydrangeas.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Jellyfish.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Koala.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Lighthouse.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Penguins.jpg" alt="...">
+                        </a>
+                    </div>
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Tulips.jpg" alt="...">
+                        </a>
+                  </div>
+
+                   <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Koala.jpg" alt="...">
+                        </a>
+                    </div>
+
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Chrysanthemum.jpg" alt="...">
+                        </a>
+                    </div>
+    
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Desert.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Hydrangeas.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Jellyfish.jpg" alt="...">
+                        </a>
+                    </div>
+            
+                    <div class="col-xs-3 col-md-2">
+                        <a href="#" class="thumbnail">
+                          <img src="/uploads/temp/Koala.jpg" alt="...">
+                        </a>
+                    </div>
+
+                </div>    
+            </div>
+        </div>
+    </section>
 </div>
