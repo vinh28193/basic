39a8d07f76ce3f59d130fe20ec0abe4850b561c7
viyt28 <vinh28193@gmail.com>
2018-03-26 20:54:45 +0700

    mirgate

--- migrations/m170713_155227_init.php
+++ migrations/m170713_155227_init.php
@@ -3,33 +3,23 @@
 use app\common\db\Migration;
 
 /**
- * Handles the creation of table `{{%application}}`.
+ * Initialization by `yii migrate`.
  */
 class m170713_155227_init extends Migration
 {
+
     /**
      * @inheritdoc
      */
     public function up()
     {
-        $this->createTable('{{%application}}', [
-            'id' => $this->primaryKey(),
-            'name' => $this->string(512)->notNull(),
-            'charset' => $this->string(12)->notNull(),
-            'language' => $this->string(12)->notNull(),
-            'time_zone' => $this->string(12)->notNull(),
-            'run_mode' => $this->string(12)->notNull(),
-            'created_at' => $this->integer(),
-            'updated_at' => $this->integer(),
-        ], $this->tableOptions);
+        echo "m170713_155227_init cannot be applied.\n";
     }
-
     /**
      * @inheritdoc
      */
     public function down()
     {
-        $this->dropTable('{{%application}}');
-
+         echo "m170713_155227_init cannot be reverted.\n";
     }
 }
--- migrations/m180111_134104_create_category_table.php
+++ migrations/m180111_134104_create_category_table.php
@@ -20,10 +20,28 @@ class m180111_134104_create_category_table extends Migration
             'id' => $this->primaryKey(),
             'title' => $this->string(512)->notNull(),
             'slug' => $this->string(1024)->notNull(),
+            'parent_id' => $this->integer(),
             'status' => $this->smallInteger()->notNull()->defaultValue(1),
             'created_at' => $this->integer(),
             'updated_at' => $this->integer(),
         ], $this->tableOptions);
+
+        // creates index for column `parent_id`
+        $this->createIndex(
+            'idx-category-parent_id',
+            '{{%category}}',
+            'parent_id'
+        );
+
+        // add foreign key for table `{{%category}}`
+        $this->addForeignKey(
+            'fk-category-parent_id',
+            '{{%category}}',
+            'parent_id',
+            '{{%category}}',
+            'id',
+            'CASCADE'
+        );
     }
 
     /**
@@ -31,6 +49,18 @@ class m180111_134104_create_category_table extends Migration
      */
     public function down()
     {
+        // drops foreign key for table `'{{%category}}'`
+        $this->dropForeignKey(
+            'fk-category-parent_id',
+            '{{%category}}'
+        );
+        
+        // drops index for column `parent_id`
+        $this->dropIndex(
+            'idx-category-parent_id',
+            '{{%category}}'
+        );
+
         $this->dropTable('{{%category}}');
     }
 }
