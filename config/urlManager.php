1414ec71ac6ad067e57f30f9afa2ba816bb75397
Vinh <vinh28193@gmail.com>
2018-06-20 22:05:51 +0700

    do nothing

--- config/urlManager.php
+++ config/urlManager.php
@@ -1,9 +1,12 @@
 <?php
 return [
-    'enablePrettyUrl' => false,
+    'enablePrettyUrl' => true,
     'showScriptName' => false,
     'rules' => [
-    
+        '<controller:[\w-]+>'                                 => '<controller>/index',
+        '<controller:[\w-]+>/<action:[\w-]+>'                 => '<controller>/<action>',
+        '<module:[\w-]+>/<controller:[\w-]+>'                 => '<module>/<controller>/index',
+        '<module:[\w-]+>/<controller:[\w-]+>/<action:[\w-]+>' => '<module>/<controller>/<action>',
     ],
 ];
 ?>
